173215b819f6acc7a620b5f4af89601cd86515f5
ThayaneBarros <thayane.barros.ext@synchro.com.br>
2026-01-27 18:19:03 -0300

    fix: ajusta uso do middleware de autenticação

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { UserController } from "./controllers/UserController";
 import { CardController } from "./controllers/CardController";
 import { FolderController } from "./controllers/FolderController";
 import { SummaryController } from "./controllers/SummaryController";
+import { authenticate } from "./middlewares/authMiddleware";
 
 dotenv.config();
 
@@ -17,37 +18,42 @@ const folderController = new FolderController();
 const summaryController = new SummaryController();
 
 // ROTAS USERS
-app.get("/api/users", userController.getAll);
-app.get("/api/users/:id", userController.getById);
 app.post("/api/users", userController.create);
-app.put("/api/users/:id", userController.update);
-app.delete("/api/users/:id", userController.delete);
+
+// Login
 app.post("/api/users/login", userController.loginUser);
 
+// USERS
+app.get("/api/users", authenticate, userController.getAll);
+app.get("/api/users/:id", authenticate, userController.getById);
+app.put("/api/users/:id", authenticate, userController.update);
+app.delete("/api/users/:id", authenticate, userController.delete);
+
 // ROTAS CARDS
-app.get("/api/cards", cardController.getAll);
-app.get("/api/cards/:id", cardController.getById);
-app.post("/api/cards", cardController.create);
-app.put("/api/cards/:id", cardController.update);
-app.delete("/api/cards/:id", cardController.delete);
+app.get("/api/cards", authenticate, cardController.getAll);
+app.get("/api/cards/:id", authenticate, cardController.getById);
+app.post("/api/cards", authenticate, cardController.create);
+app.put("/api/cards/:id", authenticate, cardController.update);
+app.delete("/api/cards/:id", authenticate, cardController.delete);
 app.get(
   "/api/cards/disciplina/:disciplina",
-  cardController.getCardsByDisciplina
+  authenticate,
+  cardController.getCardsByDisciplina,
 );
 
 // ROTAS FOLDERS
-app.get("/api/folders", folderController.getAll);
-app.get("/api/folders/:id", folderController.getById);
-app.post("/api/folders", folderController.create);
-app.put("/api/folders/:id", folderController.update);
-app.delete("/api/folders/:id", folderController.delete);
+app.get("/api/folders", authenticate, folderController.getAll);
+app.get("/api/folders/:id", authenticate, folderController.getById);
+app.post("/api/folders", authenticate, folderController.create);
+app.put("/api/folders/:id", authenticate, folderController.update);
+app.delete("/api/folders/:id", authenticate, folderController.delete);
 
 // ROTAS SUMMARIES
-app.get("/api/summaries", summaryController.getAll);
-app.get("/api/summaries/:id", summaryController.getById);
-app.post("/api/summaries", summaryController.create);
-app.put("/api/summaries/:id", summaryController.update);
-app.delete("/api/summaries/:id", summaryController.delete);
+app.get("/api/summaries", authenticate, summaryController.getAll);
+app.get("/api/summaries/:id", authenticate, summaryController.getById);
+app.post("/api/summaries", authenticate, summaryController.create);
+app.put("/api/summaries/:id", authenticate, summaryController.update);
+app.delete("/api/summaries/:id", authenticate, summaryController.delete);
 
 const PORT = process.env.PORT || 3000;
 
